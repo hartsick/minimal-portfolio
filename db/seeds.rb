1e10c3ca286d2eec20fb11f22d1f91022c8149ea
Christa Hartsock <christa.hartsock@gmail.com>
2014-07-20 19:58:29 -0700

    add user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create([{ email: 'email@test.com', password: 'change_this_password'}])
