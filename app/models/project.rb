19b22a5a0a2988b1390c65d037dc3ca4e780fd2f
Christa Hartsock <christa.hartsock@gmail.com>
2014-07-23 00:17:31 -0700

    inverse of on models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,8 @@
 class Project < ActiveRecord::Base
   has_and_belongs_to_many :categories
-  has_one :project_detail, dependent: :destroy
+  has_one :project_detail, { dependent: :destroy, inverse_of: :project}
 
-  accepts_nested_attributes_for :project_detail
+  accepts_nested_attributes_for :project_detail, :allow_destroy => true
 
   validates_presence_of :title
   validates_presence_of :number
--- app/models/project_detail.rb
+++ app/models/project_detail.rb
@@ -1,5 +1,5 @@
 class ProjectDetail < ActiveRecord::Base
-  belongs_to :project
+  belongs_to :project, inverse_of: :project_detail
 
   validates_presence_of :description
   validates_presence_of :project
