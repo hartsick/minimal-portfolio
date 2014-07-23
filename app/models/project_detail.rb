36e885017324121f796a79977345118e6ce95160
Christa Hartsock <christa.hartsock@gmail.com>
2014-07-23 00:11:37 -0700

    project accepts nested attributs for project detail

--- app/models/project_detail.rb
+++ app/models/project_detail.rb
@@ -1,8 +1,6 @@
 class ProjectDetail < ActiveRecord::Base
   belongs_to :project
 
-  accepts_nested_attributes_for :project
-
   validates_presence_of :description
   validates_presence_of :project
 end
