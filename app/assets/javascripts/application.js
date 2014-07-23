47f12e5bf620f5e5a016e2a8bc3d559bd28da051
Christa Hartsock <christa.hartsock@gmail.com>
2014-07-22 23:01:02 -0700

    style accordion

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -85,22 +85,30 @@ $(document).ready(function(){
 
   // Respond to click
   $('dt.project.clickable').click(function() {
+    var project = $(this);
     var detail = $(this).next();
 
     // If clicked item is open, close it
-    if (detail.hasClass('accordion-active')){
-      detail.slideUp();
+    if (detail.hasClass('detail-active')){
+      detail.slideUp(400, function(){
+        detail.toggleClass('underline');
+        project.toggleClass('underline');
+        project.toggleClass('project-active');
+      });
     }
     // Otherwise, close active item and open the clicked one
     else {
-      $('.accordion-active').slideUp();
-      $('.accordion-active').toggleClass('accordion-active');
+      // Deactive open items
+      $('.detail-active').slideUp();
+      $('.project-active').toggleClass('project-active');
 
+      // Activate clicked items
       detail.slideDown();
+      project.toggleClass('underline');
+      detail.toggleClass('underline');
+      project.toggleClass('project-active');
     }
-    detail.toggleClass('accordion-active');
-
-    return false;
+    detail.toggleClass('detail-active');
   });
 
 });
