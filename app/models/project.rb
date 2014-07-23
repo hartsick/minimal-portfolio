3523c2cb2c3ea0425ce9357e3ffba385710304b6
Christa Hartsock <christa.hartsock@gmail.com>
2014-07-23 00:08:30 -0700

    index view fix

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,8 +2,6 @@ class Project < ActiveRecord::Base
   has_and_belongs_to_many :categories
   has_one :project_detail, dependent: :destroy
 
-  accepts_nested_attributes_for :project_detail
-
   validates_presence_of :title
   validates_presence_of :number
 end
--- app/models/project_detail.rb
+++ app/models/project_detail.rb
@@ -1,6 +1,8 @@
 class ProjectDetail < ActiveRecord::Base
   belongs_to :project
 
+  accepts_nested_attributes_for :project
+
   validates_presence_of :description
   validates_presence_of :project
 end
