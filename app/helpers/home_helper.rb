758335aae35bbbacc5db8b85bdcb81463fdf32b6
Christa Hartsock <christa.hartsock@gmail.com>
2014-07-24 14:56:17 -0700

    support project display of numbers with less than 6 digits

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -4,4 +4,14 @@ module HomeHelper
     Category.all.order('name')
   end
 
+  def format_project_number(integer)
+    int_string = integer.to_s
+    if int_string.length < 6
+      deficit = 6 - int_string.length
+      deficit.times do |once|
+        int_string.insert(0,"0")
+      end
+    end
+    int_string
+  end
 end
